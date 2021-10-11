e51144073891648c0796cdcc19324a5e05bceca0
dirceurjunior <dirceu.r.junior@gmail.com>
2021-10-11 13:12:50 -0300

    First Commit Demo Spring Boot

---
+++ src/main/java/com/drjunior/crm/DrjcrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.drjunior.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrjcrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrjcrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drjunior/crm/DrjcrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drjunior.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrjcrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
